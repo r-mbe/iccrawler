f98f54b8d610121e3cf7074ba2a94831f56514fb
xiyx <xiyx@ickey.cn>
2017-07-27 11:54:14 +0800

    nohup.out too large more than 37G one night rolling log script

--- crawlerSZLC/run.sh
+++ crawlerSZLC/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 rm ./go-crawler-szlc
 go build -o  go-crawler-szlc crawler.go
-./go-crawler-szlc
+
+# test
+#./go-crawler-szlc
+
+
+# product  for nohup.out too large more then 40G after running one night.
+nohup  nohuprooll.sh  2>%1  |  ./logger  go-crawler-szlc  & 
