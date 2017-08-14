e93e6d54540acbe073ed097d3e402b3c63aa97c4
xiyx <xiyx@ickey.cn>
2017-08-14 15:27:52 +0800

    timer loop

--- crawlerSZLC/test/timerloop/main.go
+++ crawlerSZLC/test/timerloop/main.go
@@ -7,16 +7,31 @@ import (
 )
 
 func main() {
-	fmt.Println("Program started...")
-	start := time.Now()
 
-	// Initialise top-level context
-	ctx := context.Background()
+	stop := time.After(3 * 10 * time.Second)
+	tick := time.NewTicker(3 * time.Second)
+	defer tick.Stop()
+	for {
+		select {
+		case <-tick.C:
+			fmt.Println("Looping.....................", time.Now())
+			fmt.Println("Program started...")
+			start := time.Now()
 
-	operationWithTimeout(ctx)
+			// Initialise top-level context
+			ctx := context.Background()
 
-	elapsed := time.Since(start)
-	fmt.Printf("Program finished... It took %s \n", elapsed)
+			operationWithTimeout(ctx)
+
+			elapsed := time.Since(start)
+			fmt.Printf("Program finished... It took %s \n", elapsed)
+
+		case <-stop:
+			fmt.Println("#################All Loop done")
+			return
+		}
+	}
+
 }
 
 func operationWithTimeout(ctx context.Context) {
@@ -24,6 +39,7 @@ func operationWithTimeout(ctx context.Context) {
 
 	// Create a channel for signal handling
 	c := make(chan bool)
+	defer close(c)
 
 	// Define a cancellation after 1s in the context
 	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
@@ -49,7 +65,7 @@ func operationWithTimeout(ctx context.Context) {
 func slowOperation(c chan bool) {
 	fmt.Println("slowOperation started...")
 
-	time.Sleep(3 * time.Second)
+	time.Sleep(4 * time.Second)
 
 	c <- true
 
