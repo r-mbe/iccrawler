66dab39971f33fc0e6362998fa73b133ffddb1be
xiyx <xiyx@ickey.cn>
2017-09-19 15:31:15 +0800

    add false

--- mlcc1/mlcc1.js
+++ mlcc1/mlcc1.js
@@ -1,5 +1,6 @@
 var Nightmare = require('nightmare');
 var Promise = require("bluebird");
+let cheerio = require("cheerio");
 
 const nightmare = Nightmare({ show: true });
 
@@ -12,9 +13,7 @@ nightmare
   .wait('#content .tree')
   .click("#content > div:nth-child(3) > a")
   .wait('.modal-content')
-  // .click('#cate4')
   .goto('http://www.mlcc1.com/search_simplex.html?searchkey=&flag=4')
-  .wait('.search_list')
   .evaluate(() => {
     return Array.from(document.querySelectorAll('.search_list  a.parts_n')).map(a => a.href);
   })
@@ -36,7 +35,15 @@ nightmare
         .end()
         .then(res => {
           console.log('detail page' + res);
-          data.push(res);
+
+          // getRows(res)
+          //   .then( result => {
+          //       console.log("one row result=" + result);
+          //       data.push(result);
+          //   })
+          //   .catch(err => {
+          //       console.error('Parse Detail page error', err);
+          //   })
         })
         .catch((err => {
           console.error('Detail search failed', err);
@@ -48,3 +55,47 @@ nightmare
   .catch((error) => {
     console.error('Search failed:', error);
   });
+
+
+
+//parse one row
+
+  async function getRows(html) {
+      let $ = await cheerio.load(html);
+      let rows = $('tbody > tr').toArray();
+      let res = [];
+
+      await rows.map(async(row) => {
+          r = await parseRow($, row)
+              //console.log("=========loopppp row=", JSON.stringify(r));
+          res.push(r);
+      });
+
+      // console.log("now in parseHtml function get resturl rowsres=", res);
+      return res;
+  }
+
+  async function parseRow($, row) {
+      //remove \r\n and space with str replace
+
+      let hrows = $('tbody > tr').toArray();
+      let r = {}
+
+      let part = $(row).eq(0).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
+      let pro_maf = $(row).eq(1).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
+      let resistance = $(row).eq(2).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
+      let tolerance = $(row).eq(2).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
+      let rsize = $(row).eq(2).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
+      let temp = $(row).eq(2).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
+      let pd = $(row).eq(2).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
+
+      r.part = part;
+      r.pro_maf = pro_maf;
+      r.resistance = resistance;
+      r.tolerance = tolerance;
+      r.rsize = rsize;
+      r.temp = temp;
+      r.pd = pd;
+
+      return await r;
+}
