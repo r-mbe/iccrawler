6256f788bb1a4f0baf8a37d61d3536ddce22962f
xiyx <xiyx@ickey.cn>
2017-09-20 16:20:10 +0800

    v0.0.3 final crawler to csv full ok version

--- mlcc1/mlcc1.js
+++ mlcc1/mlcc1.js
@@ -1,11 +1,16 @@
 var Nightmare = require('nightmare');
 var Promise = require("bluebird");
 var cheerio = require("cheerio");
-var ProxM = require ('./getproxy.js')
+var ProxM = require ('./getproxy.js');
+var Papa = require('papaparse');
+var fs = require('fs');
+
+var CsvOutFile = "data.csv"
 
 
 async function getByKeyword(url) {
 
+ let fresult = false;
 
   let nightmare = undefined;
   var proxyIp = await ProxM.getProxyIps();
@@ -25,16 +30,19 @@ async function getByKeyword(url) {
      show: true })
   }
 
+
+let homeurl = 'http://www.mlcc1.com';
+
 await nightmare
-  .goto(url)
+  .useragent('Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36')
+  .goto(homeurl)
+  .wait(500)
   .click('#login_c')
   .type('#login_user_name', '13632927231')
   .type('#login_email_password', '@123456')
   .click('#login_button')
-  .wait('#content .tree')
-  // .click("#content > div:nth-child(3) > a")
-  // .wait('.modal-content')
-  .goto('http://www.mlcc1.com/search_simplex.html?searchkey=&flag=4')
+  .wait(1000)
+  .goto(url)
   .wait('.search_list')
   .evaluate(() => {
     return Array.from(document.querySelectorAll('.search_list  a.parts_n')).map(a => a.href);
@@ -42,7 +50,6 @@ await nightmare
   .end()
   .then( links => { console.log(links); return links })
   .then( links => {
-
 
      let data = [];
     return  Promise.all(links.map(async link => {
@@ -70,7 +77,9 @@ await nightmare
      }, 300);
 
        return await nightmare2
+        .useragent('Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36')
         .goto(link)
+        .wait(500)
         .wait('.red_bord')
         .evaluate(() => {
           return document.querySelector('.red_bord').innerHTML;
@@ -105,15 +114,23 @@ await nightmare
 
         // console.log('AAAAdd row onew page into html.' + r);
         console.log('AAAAdd row onew page into html.' + JSON.stringify(r));
+         drows.push(r);
 
-        // json to csv file 
-        var csv = Papa.unparse(r);
+      })).then(rows => {
+        console.log('.......all rows final parsed data' + JSON.stringify(rows));
+        console.log('.......all drows final parsed data' + JSON.stringify(drows));
 
-        return drows.push(r);
+        var csv = Papa.unparse(drows)
 
-      })).then(adata => {
-        console.log('all final parsed data' + JSON.stringify(adata));
-        return adata;
+        fs.appendFile(CsvOutFile, csv, function(err) {
+          if (err) throw err;
+          console.log('file append. saved');
+        });
+        // var csv = Papa.unparse(drows, "data.csv");
+        if (drows.length > 0 ) {
+          fresult = true;
+        }
+        return drows;
       })
     //
   })
@@ -124,13 +141,115 @@ await nightmare
     console.error('Search failed:', error);
   });
 
+   return fresult;
 }
 
 
-async function crawlering() {
-  let url = 'http://www.mlcc1.com';
+async function login(url) {
+var result = true ;
 
-  return await getByKeyword(url);
+  try {
+        let nightmare = undefined;
+        var proxyIp = await ProxM.getProxyIps();
+        if (!proxyIp.found) {
+            console.log('get da xiang proxy ip error\n');
+            proxyIp.proxyip = undefined;
+
+            nightmare = Nightmare({ show: true })
+        } else {
+          console.log('get daxing proxy ip=' + proxyIp.proxyip.toString());
+          nightmare = Nightmare({
+           switches: {
+               // 'proxy-server': '10.8.11.240:8100' // set the proxy server here ...
+               //'proxy-server': proxyIp // set the proxy server here ...
+               'proxy-server': await proxyIp.proxyip.toString() // set the proxy server here ...
+           },
+           show: true })
+        }
+
+        await nightmare
+        .useragent('Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36')
+        .goto(url)
+        .wait(500)
+        .click('#login_c')
+        .type('#login_user_name', '13632927231')
+        .type('#login_email_password', '@123456')
+        .click('#login_button')
+        // .wait('#content .tree')
+        .end()
+        .catch((error) => {
+          console.error('Search failed:', error);
+          result = false;
+        });
+
+  } catch (e) {
+      console.log('login catch error err.' + e);
+      result = false;
+  }
+
+  return result;
+}
+
+async function crawlering() {
+  // let homeurl = 'http://www.mlcc1.com';
+
+  // var islogin = false;
+
+  // islogin = await login(homeurl);
+
+
+  // if (!islogin) {
+  //   console.log('login error.')
+  //   return;
+  // }
+
+  //crawlering each
+  let baseurl = "http://www.mlcc1.com/search_simplex.html?searchkey=&flag=4";
+  let url;
+  url = baseurl;
+
+  var nums = [];
+  // Promise.mapSeries()(var i=0; i<= 112662; i++ ){
+
+  for (var i=0; i<= 112662; i++ ){
+    nums.push(i)
+  }
+
+      // return  Promise.mapSeries(links.map(async link => {
+
+
+    for (let i of nums ) {
+      if ( i > 0 ){
+        url = baseurl + "&p=" + i.toString();
+        console.log("now will cralwer page url= " + url);
+      }
+
+      try {
+        await getByKeyword(url)
+              .then(ok => {
+                if ( !ok ) {
+                  console.log('page crawler error.')
+                  fs.appendFile('crawling.log', url, function(err) {
+                    if (err) throw err;
+                    console.log('file saved');
+                  });
+                 }
+              });
+        console.log("xxxx now will finishedl  = " + url);
+        // });
+      } catch (e) {
+        console.log('page crawler error.')
+        fs.appendFile('crawling.log', url, function(err) {
+          if (err) throw err;
+          console.log('file saved');
+        });
+        return;
+      }
+
+  }
+
+  console.log("final crawlering....")
+
 }
 
  crawlering()
@@ -163,10 +282,10 @@ async function crawlering() {
       let part = $(row).eq(0).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
       let pro_maf = $(row).eq(1).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
       let resistance = $(row).eq(2).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
-      let tolerance = $(row).eq(2).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
-      let rsize = $(row).eq(2).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
-      let temp = $(row).eq(2).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
-      let pd = $(row).eq(2).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
+      let tolerance = $(row).eq(3).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
+      let rsize = $(row).eq(4).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
+      let temp = $(row).eq(5).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
+      let pd = $(row).eq(6).find('td').eq(1).text().trim().replace(/\s+/g, "").replace(/\r\n|\n/g, "");
 
       r.part = part;
       r.pro_maf = pro_maf;
---
+++ mlcc1/test.js
@@ -0,0 +1,24 @@
+var Papa = require('papaparse');
+var fs = require('fs');
+
+var csv = Papa.unparse([
+	{
+		"Column 1": "foo",
+		"Column 2": "bar"
+	},
+	{
+		"Column 1": "abc",
+		"Column 2": "def"
+	}
+]);
+
+
+fs.appendFile('file.csv', csv, function(err) {
+  if (err) throw err;
+  console.log('file saved');
+});
+
+fs.appendFile('file.csv', csv, function(err) {
+  if (err) throw err;
+  console.log('file saved');
+});
